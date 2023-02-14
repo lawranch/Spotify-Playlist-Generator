1144b561ab87ec8469258e0800054d80ed841449
Lawrance Huynh <lawranch@uci.edu>
2023-02-13 16:44:35 -0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,51 @@
+from bs4 import BeautifulSoup
+import requests
+import spotipy
+from spotipy.oauth2 import SpotifyClientCredentials, SpotifyOAuth
+
+# ---------------------------- USER INPUT ---------------------------- #
+date = input("Which date do you want to make a playlist out of? Type the date in this format YYYY-MM-DD: ")
+year = date[0:4]
+
+# ---------------------------- WEB SCRAPING ---------------------------- #
+response = requests.get(f"https://billboard.com/charts/hot-100/{date}")
+soup = BeautifulSoup(response.text, "html.parser")
+
+titles = soup.select("li ul li h3", class_="c-title")
+top_100 = [title.text.strip() for title in titles]
+
+# ---------------------------- SPOTIFY PLAYLIST ---------------------------- #
+
+SPOTIPY_CLIENT_ID = "97ff0442d90443418541cf4eb46e14ec"
+SPOTIPY_CLIENT_SECRET = "720071fad25f41fa8a422cde3b7d061b"
+SPOTIPY_REDIRECT_URI = "http://localhost:8888/callback"
+
+sp = spotipy.Spotify(
+    auth_manager=SpotifyOAuth(
+        client_id=SPOTIPY_CLIENT_ID,
+        client_secret=SPOTIPY_CLIENT_SECRET,
+        redirect_uri=SPOTIPY_REDIRECT_URI,
+        scope="playlist-modify-private",
+        cache_path="token.txt",
+        show_dialog=True,
+    )
+)
+
+user_id = sp.current_user()["id"]
+
+# ---------------------------- SEARCH SPOTIFY SONGS ---------------------------- #
+song_uris = []
+
+for song in top_100:
+    result = sp.search(q=f"track: {song} year: {year}")
+
+    try:
+        uri = result["tracks"]["items"][0]["uri"]
+        song_uris.append(uri)
+    except IndexError:
+        print(f"Skipped {song}, doesn't exist in Spotify.")
+
+# ---------------------------- ADD SONGS TO PLAYLIST ---------------------------- #
+playlist = sp.user_playlist_create(user=user_id, name=f"{date} Billboard 100", public=False)
+
+sp.playlist_add_items(playlist_id=playlist["id"], items=song_uris)
